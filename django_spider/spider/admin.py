db6a6fd290a42c8ca2eac5033185e8621dace712
Wan <wanshuo@baidu.com>
2018-08-26 20:35:14 +0800

    对 spider APP进行配置；学习完入门教程 part1,2；掌握 model 的原理和建表的操作；配置了 Django 的在线管理员权限，实现了线上增删改查数据

--- django_spider/spider/admin.py
+++ django_spider/spider/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import test_table
 # Register your models here.
+
+admin.site.register(test_table)
+
--- django_spider/spider/models.py
+++ django_spider/spider/models.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
 
-class test_table(models.Model):
-	name = models.CharField(max_length=20)
-	age = models.IntegerField()
+class test_table(models.Model): # models.Model 代表 该类是 models.Model 的子类
 	
+	def __str__(self):
+		return self.name # 作为该对象的描述
+	
+	name = models.CharField(max_length=20)
+	age = models.IntegerField(default=0)
